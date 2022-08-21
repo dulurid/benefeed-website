97bd9dc904ea7f129139d023ba27d8298ecd0436
dimasrakas <dimasrakas@gmail.com>
2022-08-21 15:57:04 +0700

    chore: fix meta

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,7 @@ useHead({
     },
     {
       property: 'og:image',
-      content: '/preview.png',
+      content: 'https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png',
     },
   ],
 })
