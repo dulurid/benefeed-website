69c29503487028b9b7a73bb728029b3ab3ae80c4
rendiwidi <rendi.widi@gmail.com>
2022-08-27 20:15:55 +0700

    add : Components\ui\LoadingBlogCard

---
+++ src/components/ui/LoadingBlogCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div
+    class="flex flex-col  col-span-12 space-y-3 shadow-xl  p-4 rounded-xl item-center bg-neutral-50 sm:col-span-6 xl:col-span-4"
+  >
+    <div class="h-4 bg-gray-200 rounded-full animate-pulse w-16" />
+
+    <div class="h-8 mt-5 mb-4 bg-gray-200 rounded-full animate-pulse w-full" />
+
+    <div class="h-3 bg-gray-200 rounded-full animate-pulse w-[300px]" />
+    <div class="h-3 mt-2 bg-gray-200 rounded-full animate-pulse w-32" />
+
+    <div class="h-6 my-8 mb-4 bg-gray-300 rounded-full animate-pulse w-24" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
